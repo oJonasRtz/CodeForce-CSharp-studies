e2729f3fec88c8eea1416ae23005a0df7d7425aa
oJonasRtz <jonasper19@gmail.com>
2026-03-11 21:20:55 +0000

    var exercise

---
+++ vars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+	static void Print(object[] data)
+	{
+		foreach (object item in data)
+		{
+			if (item == null)
+				continue;
+			if (item is not IEnumerable collection || item is string) {
+				Console.WriteLine($"{item} (Type: {item.GetType()})");
+				continue;
+			}
+
+			Console.WriteLine($"Collection (Type: {item.GetType()}):");
+			foreach (var element in collection)
+				Console.WriteLine($"  - {element}");
+		}
+	}
+	static void Main()
+	{
+		object[] myVars =
+		[
+			42,
+			"42",
+			3.14,
+			true,
+			new List<int> { 1, 2, 3 },
+			new Dictionary<string, int> { {"one", 1}, {"two", 2} },
+			(42, "forty-two"), //tuple
+			new HashSet<int> { 1, 2, 3 },
+			new int[] { 1, 2, 3 },
+		];
+		
+		Print(myVars);
+	}
+}
---
+++ vars/Readme.md
@@ -0,0 +1,21 @@
+# Exercício — Estruturas de dados em C#
+
+### Crie um programa que:
+1. Crie diferentes estruturas de dados  
+2. Armazene elas em uma coleção   
+3. Percorra essa coleção  
+4. Imprima:
+    - valor
+    - tipo
+    - tamanho (se aplicável)
+
+### Estruturas que você deve usar
+- `int`
+- `string`
+- `double`
+- `bool`
+- `List<int>`
+- `Dictionary<string, int>`
+- `Tuple`
+- `HashSet<int>`
+- `int[] (array)`
